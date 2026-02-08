609d4732beae5d183a1b02db2f3e8c2426fc5e61
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2026-02-08 19:58:49 +0200

    refactor: avoid overriding the Vite output directory
    This is so all build commands will produce the default and expected
    output in frontend/dist, and the Dockerfile will copy from there to
    backend/public. This way we can also have a separate build command for
    local development that doesn't require building the frontend into the
    backend's public directory.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Configure Vite to build directly to backend public directory
-  build: {
-    outDir: "../backend/public",
-    emptyOutDir: false, // Don't empty entire directory, only overwrite build artifacts
-  },
-  // If we want to build a local version (that uses local services)
-  // define: {
-  // 	'process.env.VITE_LOCAL': 'true'
-  // }
 });
